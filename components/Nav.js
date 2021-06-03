d5d5e6cde9f72d9d9f003d5062368114aa4b227c
bishkou <chedyhammami@yahoo.com>
2021-06-03 10:08:52 +0200

    fix

--- components/Nav.js
+++ components/Nav.js
@@ -5,6 +5,7 @@ import {useRouter} from "next/router";
 const Nav = ({title, Icon}) => {
     const router = useRouter();
     return (
+
         <nav className='relative'>
             <div className='flex px-10 sm:px-20 text-2xl whitespace-nowrap
             space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
